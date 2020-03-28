958046762127a433311e48c03dabddc904fa79b9
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-03-28 15:57:40 +0700

    Insert single and multiple records

--- employees.js
+++ employees.js
@@ -25,3 +25,22 @@ connection.query(sqlAlterTable, (err, result) => {
     if (err) throw err;
     console.log('Table altered');
 });
+
+var sqlInsertRecord = "INSERT INTO employees (id, name, age, city, salary) VALUES ('1', 'Erik Rio Setiawan', '22', 'Ponorogo', '900000000')";
+
+connection.query(sqlInsertRecord, (err, result) => {
+    if (err) throw err;
+    console.log('1 record inserted');
+});
+
+var sqlMultipleInsertRecord = "INSERT INTO employees VALUES ?";
+var values = [
+    ['2', 'Name 2', '20', 'New York', '35840'],
+    ['3', 'Name 3', '21', 'Silicon Valley', '54634'],
+    ['4', 'Name 4', '28', 'California', '8349']
+];
+connection.query(sqlMultipleInsertRecord, [values], (err, result) => {
+    if (err) throw err;
+    console.log('Number of record inserted: ' + result.affectedRows);
+    console.log(result);
+});
